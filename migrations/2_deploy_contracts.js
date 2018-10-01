76b5d303c1ac3394f8ac1d4c38e33e0f64aa2216
matr1xc0in <>
2018-09-30 20:49:35 -0700

    Refactor migration code, register reward contract during deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,29 @@
+/*jshint esversion: 6*/
 var erc20 = artifacts.require("ERC20Token");
 var ipfsreward = artifacts.require("RewardDistributor.sol");
 var trade = artifacts.require("TradeContract.sol");
 var proof_of_stake_balance = 100;
+var ipfsreward_instance;
+var erc20_instance;
+var trade_instance;
 
 module.exports = function(deployer) {
-  deployer.deploy(erc20, 'BlockMed', 'BMD').then(function() {
-    console.log("ERC20Token NoSilo address is created on " + erc20.address);
-    return deployer.deploy(ipfsreward, erc20.address, true, proof_of_stake_balance);
+  deployer.deploy(erc20, 'BlockMed', 'BMD').then(function(erc20_i) {
+    erc20_instance = erc20_i;
+    console.log("ERC20Token BMD address is created on " + erc20_instance.address);
+    console.log("Don't forget to register the RewardDistributor address for Escrow");
+    return deployer.deploy(ipfsreward, erc20_instance.address, true, proof_of_stake_balance);
+  }).then(function(ipfsreward_i) {
+    ipfsreward_instance = ipfsreward_i;
+    console.log("RewardDistributor address is created on " + ipfsreward_instance.address);
+    console.log("Don't forget to fund RewardDistributor contract " + ipfsreward_instance.address + " 500000000 tokens to start from accounts[0]");
+    return deployer.deploy(trade, erc20_instance.address, true);
+  }).then(function(trade_i) {
+    console.log("TradeContract address is created on " + trade_i.address);
+    console.log("Don't forget to fund TradeContract contract " + trade_i.address + " 500000000 tokens to start from accounts[0]");
   }).then(function() {
-    console.log("RewardDistributor address is created on " + ipfsreward.address);
-    console.log("Don't forget to fund RewardDistributor contract " + ipfsreward.address + " 500000000 tokens to start from accounts[0]");
-    return deployer.deploy(trade, erc20.address, true);
-  }).then(function() {
-    console.log("TradeContract address is created on " + trade.address);
-    console.log("Don't forget to fund TradeContract contract " + trade.address + " 500000000 tokens to start from accounts[0]");
+    erc20_instance.register_rewardcontract(ipfsreward_instance.address);
   });
+
+  
 };
