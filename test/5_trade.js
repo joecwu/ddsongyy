b4b97e371ab117cb829dd7906cd5f888dd65a20b
matr1xc0in <>
2018-09-30 18:39:27 -0700

    Fix trading contract issue due to decimals changes. Make exchange rate flexible and delegate modification permission to separate wallet

--- test/5_trade.js
+++ test/5_trade.js
@@ -114,6 +114,7 @@ contract('Trade', function(accounts) {
     let eventCounter = {}; // to track all events fired
     let erc20_contract = null;
     let trade_contract = null;
+    let pitmaster = publicKeys[4]; // the wallet that can modify the exchange rate
 
     /* jshint ignore:start */
     before(async () => {
@@ -134,6 +135,11 @@ contract('Trade', function(accounts) {
       if (publicKeys[0] !== owner || publicKeys[1] !== other) {
         throw new Error('Use `truffle develop` and store the keys in ./test/truffle-keys.js' +
         ', and make sure you specify these keys in ganache-cli');
+      }
+
+      if (publicKeys[4] === owner) {
+        throw new Error('The 4th publicKeys[4] shall not be the same as the owner that deploys this contract' +
+        ', we use publicKeys[4] for testing in our test cases for such purposes.');
       }
   
       // Tracks all events for later verification, count may be sufficient?
@@ -157,14 +163,16 @@ contract('Trade', function(accounts) {
       let a0 = (await erc20_contract.balanceOf.call(accounts[0])).toNumber();
       let e0 = (await erc20_contract.balanceOf.call(erc20_contract.address)).toNumber();
       let t0 = (await erc20_contract.balanceOf.call(trade_contract.address)).toNumber();
+      let initEthbalance = web3.eth.getBalance(publicKeys[5]);
       logging('accounts[0]=' + accounts[0] + ' has start token balance ' + a0);
       logging('erc20_contract.address=' + erc20_contract.address + ' has start token balance ' + e0);
       logging('trade_contract.address=' + trade_contract.address + ' has start token balance ' + t0);
       logging('publicKeys[5]=' + notOwner + ' has start token balance ' + notOwnerBalanceBefore);
+      logging('publicKeys[5]=' + notOwner + ' has init Ether balance ' + initEthbalance);
       logging('TradeContract contract address ' + trade_contract.address + ' has init Ether balance ' + web3.eth.getBalance(trade_contract.address));
       // assert.equal(t0, pre_fund_amount, "trader contract should be pre-funded with " + pre_fund_amount + " tokens from accounts[0]=" + accounts[0]);
 
-      let value = 1; // 1 eth = 1 * 10 ** 18 wei. This needs to align with the contract
+      let value = 1 * 10 ** 18; // 1 eth = 1 * 10 ** 18 wei.
 
       let data = web3Contract.takerBuyAsset.getData();
 
@@ -181,14 +189,45 @@ contract('Trade', function(accounts) {
       e0 = (await erc20_contract.balanceOf.call(erc20_contract.address)).toNumber();
       t0 = (await erc20_contract.balanceOf.call(trade_contract.address)).toNumber();
       logging(notOwner + ' has new token balance ' + notOwnerBalanceAfter);
+      assert(notOwnerBalanceAfter, eth_to_tok_exchangeRate * 10**decimals, "should have new token balance " + eth_to_tok_exchangeRate * 10**decimals);
       logging('accounts[0]=' + accounts[0] + ' has new token balance ' + a0);
       logging('erc20_contract.address=' + erc20_contract.address + ' has new token balance ' + e0);
       logging('trade_contract.address=' + trade_contract.address + ' has new token balance ' + t0);
-      logging('RewardDistributor contract address ' + trade_contract.address + ' has new Ether balance ' + web3.eth.getBalance(trade_contract.address));
-
+      logging('publicKeys[5]=' + notOwner + ' has new Ether balance ' + web3.eth.getBalance(publicKeys[5]));
+      assert(web3.eth.getBalance(trade_contract.address), value, "Trading contract received eth is not the same as " + value);
+      assert.isAtMost(web3.eth.getBalance(publicKeys[5]), (initEthbalance - value), "The full " + value + " did not reach the Trading contract");
       // assert.equal(notOwnerBalanceAfter, 10000, 'it should get 10000 tokens for 1 eth');
       // assert.equal(t0, 499990000, 'trader contract token should subtract 10000');
       assert.strictEqual(0, result.indexOf('0x'));
+    });
+
+    it('should NOT be able to set exchange rate before it is delegated', async function() {
+      let tryCatch = require("./exceptions.js").tryCatch;
+      let errTypes = require("./exceptions.js").errTypes;
+      // Should trigger a revert
+      await tryCatch(trade_contract.setExchangeRate(999) , errTypes.revert);
+      let ex_rate = (await trade_contract.exchangeRate.call()).toNumber();
+      logging("Trading contract current exchange rate is " + ex_rate);
+      assert(ex_rate, eth_to_tok_exchangeRate, "exchange rate shall not change before delegation occurs!");
+      (await trade_contract.delegateExchangerAddress(publicKeys[4]));
+      eth_to_tok_exchangeRate = 999;
+      let shall_pass_result = (await trade_contract.setExchangeRate(eth_to_tok_exchangeRate, {from: accounts[4]}));
+      logging(shall_pass_result);
+      let new_ex_rate = (await trade_contract.exchangeRate.call()).toNumber();
+      assert(new_ex_rate, eth_to_tok_exchangeRate, "exchange rate shall not change before delegation occurs!");
+      logging("Trading contract new exchange rate is " + new_ex_rate);
+      let prev_ex_rate = eth_to_tok_exchangeRate;
+      eth_to_tok_exchangeRate = 200;
+      // This should fail, even it is triggered from the contract owner or any other non-dedicated wallet
+      await tryCatch(trade_contract.setExchangeRate(eth_to_tok_exchangeRate, {from: accounts[3]}) , errTypes.revert);
+      await tryCatch(trade_contract.setExchangeRate(eth_to_tok_exchangeRate, {from: accounts[2]}) , errTypes.revert);
+      await tryCatch(trade_contract.setExchangeRate(eth_to_tok_exchangeRate, {from: accounts[0]}) , errTypes.revert);
+      new_ex_rate = (await trade_contract.exchangeRate.call()).toNumber();
+      assert(new_ex_rate, prev_ex_rate, "exchange rate shall not change if it was triggered by the contract owner!");
+      shall_pass_result = (await trade_contract.setExchangeRate(eth_to_tok_exchangeRate, {from: accounts[4]}));
+      logging(shall_pass_result);
+      new_ex_rate = (await trade_contract.exchangeRate.call()).toNumber();
+      assert(new_ex_rate, eth_to_tok_exchangeRate, "exchange rate shall not change before delegation occurs!");
     });
     /* jshint ignore:end */
 
